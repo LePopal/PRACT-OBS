8fa8ed90bc7529fa99f1d38f720f6d29fdee1386
Popal <popal@sboub.com>
2020-05-04 17:28:12 +0200

    feat: the Key option is now obfuscated like a password

--- PRACT_OBS/OptionsForm.Designer.cs
+++ PRACT_OBS/OptionsForm.Designer.cs
@@ -58,6 +58,7 @@
             this.txtKey.Name = "txtKey";
             this.txtKey.Size = new System.Drawing.Size(990, 31);
             this.txtKey.TabIndex = 1;
+            this.txtKey.UseSystemPasswordChar = true;
             // 
             // label2
             // 
